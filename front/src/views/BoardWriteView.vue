8f4150485c3380115ddec708590499afb963c3ac
JONGSOO <24ssafy@gmail.com>
2024-11-14 17:57:04 +0900

    24-11-14 퇴실(종수vue작성)

---
+++ front/src/views/BoardWriteView.vue
@@ -0,0 +1,273 @@
+<template>
+    <div class="board-write">
+      <img class="background-img" src="background-img0.png" alt="background" />
+      <div class="content-box">본문 내용을 입력하세요.</div>
+      <div class="div">
+        <div class="div">
+          <div class="box"></div>
+          <div class="text">확인</div>
+          <img class="div2" src="div2.png" alt="icon" />
+        </div>
+      </div>
+      <div class="title">
+        <div class="div3">제목</div>
+        <div class="div4">
+          <span>
+            <span class="div-4-span">제목을 입력하세요.</span>
+            <span class="div-4-span2"></span>
+          </span>
+        </div>
+      </div>
+      <div class="file-attach">
+        <div class="attach-buttton">
+          <div class="attach-button-box"></div>
+          <div class="attach-button-text">내 pc</div>
+        </div>
+        <div class="div5">파일 첨부</div>
+        <img class="vector-6" src="vector-60.svg" alt="vector" />
+      </div>
+      <img class="write-form-box" src="write-form-box0.svg" alt="write form" />
+      <div class="magazine-search">
+        <div class="search-box"></div>
+        <img class="search-icon" src="search-icon0.png" alt="search icon" />
+        <div class="search-text">커뮤니티 검색</div>
+      </div>
+      <div class="community-logo">Community</div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "BoardWrite",
+  };
+  </script>
+  
+  <style scoped>
+  .board-write,
+  .board-write * {
+    box-sizing: border-box;
+  }
+  .board-write {
+    height: 1644px;
+    position: relative;
+  }
+  .background-img {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    right: 0%;
+    left: 0%;
+    bottom: 0%;
+    top: 0%;
+    object-fit: cover;
+  }
+  .content-box {
+    color: #9a9a9a;
+    text-align: left;
+    font-family: "Inter-Medium", sans-serif;
+    font-size: 24px;
+    font-weight: 500;
+    position: absolute;
+    left: 84px;
+    top: 527px;
+    width: 323px;
+    height: 43px;
+  }
+  .div {
+    width: 101px;
+    height: 43px;
+    position: static;
+  }
+  .box {
+    background: #205df8;
+    border-style: solid;
+    border-color: #002bd9;
+    border-width: 1px;
+    width: 101px;
+    height: 43px;
+    position: absolute;
+    left: 1750px;
+    top: 383px;
+  }
+  .text {
+    color: #ffffff;
+    text-align: left;
+    font-family: "Inter-Bold", sans-serif;
+    font-size: 18px;
+    font-weight: 700;
+    position: absolute;
+    left: 1764px;
+    top: 394px;
+    width: 59px;
+    height: 20px;
+  }
+  .div2 {
+    width: 18px;
+    height: 23px;
+    position: absolute;
+    left: 1817px;
+    top: 393px;
+    object-fit: cover;
+  }
+  .title {
+    width: 1569px;
+    height: 33px;
+    position: static;
+  }
+  .div3 {
+    color: #ffffff;
+    text-align: left;
+    font-family: "Inter-Bold", sans-serif;
+    font-size: 24px;
+    font-weight: 700;
+    position: absolute;
+    left: 86px;
+    top: 393px;
+    width: 105px;
+    height: 20px;
+  }
+  .div4 {
+    text-align: left;
+    font-family: "Inter-Medium", sans-serif;
+    font-size: 18px;
+    font-weight: 500;
+    position: absolute;
+    left: 226px;
+    top: 398px;
+    width: 1429px;
+    height: 28px;
+    -webkit-text-stroke: 1px #000000;
+  }
+  .div-4-span {
+    color: #a3a3a3;
+    font-family: "Inter-Medium", sans-serif;
+    font-size: 18px;
+    font-weight: 500;
+  }
+  .div-4-span2 {
+    color: #ffffff;
+    font-family: "Inter-Medium", sans-serif;
+    font-size: 18px;
+    font-weight: 500;
+  }
+  .file-attach {
+    width: 193px;
+    height: 30px;
+    position: static;
+  }
+  .attach-buttton {
+    width: 61px;
+    height: 28px;
+    position: static;
+  }
+  .attach-button-box {
+    background: #373737;
+    border-radius: 4px;
+    border-style: solid;
+    border-color: #8f8f8f;
+    border-width: 2px;
+    width: 61px;
+    height: 28px;
+    position: absolute;
+    left: 218px;
+    top: 456px;
+  }
+  .attach-button-text {
+    color: #a6a6a6;
+    text-align: left;
+    font-family: "Inter-Medium", sans-serif;
+    font-size: 16px;
+    font-weight: 500;
+    position: absolute;
+    left: 228px;
+    top: 461px;
+    width: 48px;
+    height: 20px;
+  }
+  .div5 {
+    color: #ffffff;
+    text-align: left;
+    font-family: "Inter-Bold", sans-serif;
+    font-size: 24px;
+    font-weight: 700;
+    position: absolute;
+    left: 86px;
+    top: 454px;
+    width: 148px;
+    height: 24px;
+  }
+  .vector-6 {
+    width: 6px;
+    height: 12px;
+    position: absolute;
+    left: 190px;
+    top: 464px;
+    overflow: visible;
+  }
+  .write-form-box {
+    width: 1793px;
+    height: 1231px;
+    position: absolute;
+    left: calc(50% - 890.5px);
+    top: 368px;
+    overflow: visible;
+  }
+  .magazine-search {
+    width: 407px;
+    height: 50px;
+    position: static;
+  }
+  .search-box {
+    background: rgba(255, 255, 255, 0.15);
+    border-radius: 25px;
+    border-style: solid;
+    border-color: rgba(255, 255, 255, 0.4);
+    border-width: 1px;
+    width: 21.19%;
+    height: 3.04%;
+    position: absolute;
+    right: 3.64%;
+    left: 75.17%;
+    bottom: 83.03%;
+    top: 13.93%;
+  }
+  .search-icon {
+    width: 1.46%;
+    height: 1.64%;
+    position: absolute;
+    right: 22.12%;
+    left: 76.42%;
+    bottom: 83.7%;
+    top: 14.66%;
+    object-fit: cover;
+  }
+  .search-text {
+    color: #ffffff;
+    text-align: left;
+    font-family: "Inter-Medium", sans-serif;
+    font-size: 20px;
+    font-weight: 500;
+    position: absolute;
+    right: 15.3%;
+    left: 78.66%;
+    width: 6.04%;
+    bottom: 83.7%;
+    top: 14.84%;
+    height: 1.46%;
+  }
+  .community-logo {
+    color: #ffffff;
+    text-align: left;
+    font-family: "Inter-Black", sans-serif;
+    font-size: 55px;
+    font-weight: 900;
+    position: absolute;
+    right: 78.08%;
+    left: 3.64%;
+    width: 18.27%;
+    bottom: 83.03%;
+    top: 13.26%;
+    height: 3.71%;
+  }
+  </style>
+  
---
+++ front/src/views/LoginView.vue
@@ -0,0 +1,241 @@
+ <template>
+    <div class="login">
+      <!-- 배경 이미지 -->
+      <div class="login-picture"></div>
+      
+      <!-- 로그인 영역 -->
+      <div class="sign-in">
+        <div class="div"></div>
+        <div class="social-login">
+          <img class="naver" src="@/assets/naver0.png" />
+          <img class="kakao" src="@/assets/kakao0.png" />
+          <img class="google" src="@/assets/google0.png" />
+        </div>
+        
+        <!-- 회원가입 버튼 -->
+        <div class="sign-up">
+          <div class="sign-up-box"></div>
+          <div class="sign-up-text">회원가입</div>
+        </div>
+        
+        <!-- 로그인 버튼 -->
+        <div class="login2">
+          <div class="login-box"></div>
+          <div class="login-text">로그인</div>
+        </div>
+        
+        <!-- 비밀번호 찾기 -->
+        <div class="div2">비밀번호를 잊어버리셨나요?</div>
+        
+        <!-- 비밀번호 입력 필드 -->
+        <div class="password">
+          <div class="password-box"></div>
+          <div class="password-text">비밀번호</div>
+        </div>
+        
+        <!-- 아이디 입력 필드 -->
+        <div class="id">
+          <div class="id-box"></div>
+          <div class="id-text">아이디</div>  
+        
+        <!-- 로그인 텍스트 -->
+        <div class="sign-in-logo">Sign in</div>
+      </div>
+      
+      <!-- MRT 로고 -->
+      <div class="mrt-logo">MRT</div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { defineComponent } from 'vue';
+  
+  // 필요한 컴포넌트나 라이브러리가 있다면 여기에 import 할 수 있습니다.
+  </script>
+  
+  <style scoped>
+  .login,
+  .login * {
+    box-sizing: border-box;
+  }
+  
+  .login {
+    height: 100vh; /* 화면 크기에 맞게 설정 */
+    position: relative;
+  }
+  
+  .login-picture {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-image: url('@/assets/login-picture0.png'); /* 배경 이미지 경로 */
+    background-size: cover;
+    background-position: center;
+  }
+  
+  .sign-in {
+    width: 649px;
+    height: 694px;
+    position: static;
+  }
+  
+  .div {
+    background: rgba(0, 0, 0, 0.8);
+    border-radius: 12px;
+    opacity: 0.8;
+    width: 33.8%;
+    height: 42.21%;
+    position: absolute;
+    right: 31.82%;
+    left: 34.38%;
+    bottom: 27.13%;
+    top: 30.66%;
+  }
+  
+  .social-login {
+    width: 303px;
+    height: 70px;
+    position: static;
+  }
+  
+  .naver,
+  .kakao,
+  .google {
+    width: 3.65%;
+    height: 4.26%;
+    position: absolute;
+    object-fit: cover;
+  }
+  
+  .naver {
+    right: 46.77%;
+    left: 49.58%;
+    bottom: 32.48%;
+    top: 63.26%;
+  }
+  
+  .kakao {
+    right: 52.97%;
+    left: 43.39%;
+    bottom: 32.48%;
+    top: 63.26%;
+  }
+  
+  .google {
+    right: 40.83%;
+    left: 55.52%;
+    bottom: 32.48%;
+    top: 63.26%;
+  }
+  
+  .sign-up,
+  .login2 {
+    width: 237px;
+    height: 56px;
+    position: static;
+  }
+  
+  .sign-up-box,
+  .login-box {
+    background: #ffffff;
+    border-radius: 28px;
+    width: 12.34%;
+    height: 3.41%; 
+    position: absolute;
+    bottom: 39.6%;
+    top: 57%;
+  }
+  
+  .sign-up-text,
+  .login-text {
+    color: #000000;
+    text-align: center;
+    font-family: "Inter-Bold", sans-serif;
+    font-size: 20px;
+    font-weight: 700;
+    position: absolute;
+    bottom: 40.57%;
+    top: 58.03%;
+    height: 1.4%;
+  }
+  
+  .div2 {
+    color: #ffffff;
+    text-align: left;
+    font-family: "Inter-Italic", sans-serif;
+    font-size: 18px;
+    font-weight: 400;
+    font-style: italic;
+    position: absolute;
+    right: 35.83%;
+    left: 53.07%;
+    width: 11.09%;
+    bottom: 48.44%;
+    top: 49.98%;
+    height: 1.58%;
+  }
+  
+  .password,
+  .id {
+    width: 501.62px;
+    height: 41.02px;
+    position: static;
+  }
+  
+  .password-box,
+  .id-box {
+    background: #ffffff;
+    border-radius: 20px;
+    width: 26.13%;
+    height: 2.5%;
+    position: absolute;
+  }
+  
+  .password-text,
+  .id-text {
+    color: #000000;
+    text-align: left;
+    font-family: "Inter-Italic", sans-serif;
+    font-size: 20px;
+    font-weight: 400;
+    font-style: italic;
+    position: absolute;
+    bottom: 51.78%;
+    top: 46.88%;
+    height: 1.34%;
+  }
+  
+  .sign-in-logo {
+    color: #ffffff;
+    text-align: left;
+    font-family: "Inter-BlackItalic", sans-serif;
+    font-size: 48px;
+    font-weight: 900;
+    font-style: italic;
+    position: absolute;
+    right: 43.85%;
+    left: 46.72%;
+    width: 9.43%;
+    bottom: 59.85%;
+    top: 35.46%;
+    height: 4.68%;
+  }
+  
+  .mrt-logo {
+    color: #ffffff;
+    text-align: left;
+    font-family: "Notable-Regular", sans-serif;
+    font-size: 61px;
+    font-weight: 400;
+    position: absolute;
+    right: 88.54%;
+    left: 2.4%;
+    width: 9.06%;
+    bottom: 93.07%;
+    top: 2.43%;
+    height: 4.5%;
+  }
+  </style>
+  
