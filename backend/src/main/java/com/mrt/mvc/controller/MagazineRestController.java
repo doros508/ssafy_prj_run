d32fba2f3fb307215cf274eaba058b6ce6a29779
gunwoochae <gunwoochae@naver.com>
2024-11-21 14:13:35 +0900

    24-11-21(목) 매거진 리스트 초안, 매거진 상세 전

--- backend/src/main/java/com/mrt/mvc/controller/MagazineRestController.java
+++ backend/src/main/java/com/mrt/mvc/controller/MagazineRestController.java
@@ -1,8 +1,16 @@
 package com.mrt.mvc.controller;
 
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mrt.mvc.model.dto.Magazine;
+import com.mrt.mvc.model.dto.SearchCondition;
 import com.mrt.mvc.model.service.MagazineService;
 
 @RestController
@@ -13,6 +21,16 @@ public class MagazineRestController {
 	private final MagazineService service;
 	public MagazineRestController(MagazineService service) {
 		this.service = service;
+	}
+	
+	@GetMapping("/magazine")
+	public ResponseEntity<List<Magazine>> list(@ModelAttribute SearchCondition condition) {
+		List<Magazine> list = service.getMagazineList(condition);
+		if (list == null || list.size() == 0) {
+			System.out.println("리스트가 비어있습니다.");
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+		return new ResponseEntity<>(list, HttpStatus.OK);
 	}
 	
 }
--- backend/src/main/java/com/mrt/mvc/model/dao/MagazineDao.java
+++ backend/src/main/java/com/mrt/mvc/model/dao/MagazineDao.java
@@ -1,5 +1,12 @@
 package com.mrt.mvc.model.dao;
 
-public interface MagazineDao {
+import java.util.List;
 
+import com.mrt.mvc.model.dto.Magazine;
+import com.mrt.mvc.model.dto.SearchCondition;
+
+public interface MagazineDao {
+	
+	// 게시글 전체 조회 및 검색 조회
+	List<Magazine> selectAll(SearchCondition condition);
 }
